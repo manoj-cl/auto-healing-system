011fac7b18233d3b67bdb0ef71b3be60e5b326f2
Manoj R Rao <manojrrao@Manojs-MacBook-Air.local>
2026-07-14 20:44:55 +0530

    feat(terraform): provision ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,35 @@ resource "aws_key_pair" "auto_healer_key" {
     }
 }
 
+# ------------------- EC2 INSTANCE -------------------
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["099720109477"] # Canonical's owner ID for Ubuntu
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "architecture"
+        values = ["x86_64"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+resource "aws_instance" "auto_healer" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = var.instance_type
+    key_name = aws_key_pair.auto_healer_key.key_name
+    vpc_security_group_ids = [aws_security_group.auto_healer_sg.id]
+    associate_public_ip_address = true
+    tags = {
+        Name = "auto-healer-instance"
+    }
+}
