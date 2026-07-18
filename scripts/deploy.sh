04f35fc212b5a0e71ed29a9ceb83606982ef0da7
Manoj R Rao <manojrrao@Manojs-MacBook-Air.local>
2026-07-18 23:16:50 +0530

    ci: deploy application from Github Actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,26 +9,27 @@ echo "========================================"
 cd terraform
 terraform apply -auto-approve
 
+cd ../scripts
 echo ""
 echo "Generating Ansible inventory..."
 ./generate_inventory.sh
 
-IP=$(terraform output -raw instance_public_ip)
+# IP=$(terraform output -raw instance_public_ip)
 
-echo ""
-echo "Waiting for SSH..."
+# echo ""
+# echo "Waiting for SSH..."
 
-until ssh -o StrictHostKeyChecking=no \
-          -o ConnectTimeout=5 \
-          -i ~/.ssh/id_ed25519 \
-          ubuntu@$IP "echo 'SSH Ready. (Inside the server)'" >/dev/null 2>&1
+# until ssh -o StrictHostKeyChecking=no \
+#           -o ConnectTimeout=5 \
+#           -i ~/.ssh/id_ed25519 \
+#           ubuntu@$IP "echo 'SSH Ready.'" >/dev/null 2>&1
 
-do 
-    echo "EC2 not ready yet..."
-    sleep 10
-done
+# do 
+#     echo "EC2 not ready yet..."
+#     sleep 10
+# done
 
-echo "SSH is ready. (From the local)"
+# echo "SSH is ready. (From the local)"
 
 cd ../ansible
 
